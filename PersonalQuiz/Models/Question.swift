84049ab79992d54f5ee4c9abd2c17099e728b2bb
YuriyGubin <iosdevelop.2022@gmail.com>
2022-10-06 22:33:00 +0500

    Add Model

---
+++ PersonalQuiz/Models/Question.swift
@@ -0,0 +1,43 @@
+//
+//  Question.swift
+//  PersonalQuiz
+//
+//  Created by Yuriy on 06.10.2022.
+//
+
+struct Question {
+    let title: String
+    let responseType: ResponseType
+    let answers: [Answer]
+}
+
+enum ResponseType {
+    case single
+    case multiple
+    case range
+}
+
+struct Answer {
+    let title: String
+    let animal: Animal
+}
+
+enum Animal: Character {
+    case dog = "🐶"
+    case cat = "🐱"
+    case rabbit = "🐰"
+    case turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "Вам нравится быть с друзьями. Вы окружаете себя людьми, которые вам нравятся и всегда готовы помочь."
+        case .cat:
+            return "Вы себе на уме. Любите гулять сами по себе. Вы цените одиночество."
+        case .rabbit:
+            return "Вам нравится все мягкое. Вы здоровы и полны энергии."
+        case .turtle:
+            return "Ваша сила - в мудрости. Медленный и вдумчивый выигрывает на больших дистанциях."
+        }
+    }
+}
