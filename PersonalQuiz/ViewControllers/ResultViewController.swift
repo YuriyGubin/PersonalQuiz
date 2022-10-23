f3818d0abc0d5e4a9d816c025a0b14081537ac4c
YuriyGubin <iosdevelop.2022@gmail.com>
2022-10-23 22:51:36 +0500

    Some code

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -35,7 +35,6 @@ extension ResultViewController {
         for animal in animals {
             chosenAnimals[animal] = (chosenAnimals[animal] ?? 0) + 1
         }
-        
         guard let yourAnimal = chosenAnimals.sorted(by: { $0.value > $1.value }).first?.key else { return .rabbit}
         return yourAnimal
     }
