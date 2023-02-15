fc5897b5d0627731d88493178629c87fbe109768
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-15 22:07:27 +0500

    Config done button

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -11,12 +11,22 @@ class ResultViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        navigationController?.dismiss(animated: true)
+        
+        // Work normal
+        //dismiss(animated: true)
+        
+        // If you have'nt navigation controller
+        //view.window?.rootViewController?.dismiss(animated: true)
     }
+    
+    // Check View Controller unloaded from memory
+//    deinit {
+//        print("\(type(of: self)) has been deallocated")
+//    }
     
 
 }
