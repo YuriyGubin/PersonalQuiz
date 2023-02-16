4157cd55de404fc5eb4cf1b29473d43ad49c53dc
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-16 18:12:00 +0500

    Transfer array with answers to result view controller

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -41,6 +41,11 @@ class QuestionsViewController: UIViewController {
         super.viewDidLoad()
         updateUI()
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let resultVC = segue.destination as? ResultViewController else { return }
+        resultVC.answers = answersChosen
+    }
 
 
     @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
